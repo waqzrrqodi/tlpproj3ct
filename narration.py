96d00464bba27fd375d3f7999af2eeebe29c19e7
Oskar-fluga-bzzz <oskar.hughes@skola.taby.se>
2022-12-14 11:39:01 +0100

    öhhh

---
+++ narration.py
@@ -0,0 +1,39 @@
+#introduction
+
+intro_txt1 = '''
+You wake up in the middle of a street, surrounded by dilapidated buildings and rubbish. 
+After looking around you realize that the area is completely empty. 
+The rain weighs down on your shoulders and it grows heavier with every second that passes. 
+You take a few steps forward and notice a sign that reads “Rimbo Bus Station”. 
+The words make you fall to your knees, and you realize that you are in the middle of Sweden’s 
+most dangerous city. You build up the courage to stand back up. The only path is forward.
+'''
+
+intro_txt2 = '''
+You notice a monitor below the sign, it displays the timetable for bus arrivals. 
+The only trip is to Norrtälje, and the time of departure is suspiciously missing. 
+Apart from that you see a dark alley and a rickety bridge. You have to choose between 
+a bus that may never arrive, a mysterious dark alley and a rickety bridge. What will you do?
+'''
+
+
+#Route 1 - "Wait for the bus"
+
+r1_txt1 = '''
+You decide to wait for the bus. You stand there in the cold for hours on end, 
+until you see a pair of headlights in the distance. A bus approaches and a group 
+of rogue goblins hop out. They run towards you and a fight breaks out.
+'''
+
+r1_txt2 = '''
+You won the battle and the goblins befriend you out of respect, they offer to take you to their leader, 
+you agree to follow them. You arrive at their hideout and are met with a strange man named Fulcrum.
+He takes a sip from his vape and offers an alliance. Before you get to speak to him any further a 
+group of bandits storm into the room. You have three choices. You can fight for the bandits, 
+take the side of Fulcrum or escape, what will you do?
+'''
+
+
+#Route 2 - "The Dark Alley"
+
+r2_txt1
