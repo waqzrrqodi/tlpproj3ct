bb91d0350166f7578573fca041669a7206feac2f
Sebastian Jensen <111466887+HEXtriple@users.noreply.github.com>
2023-01-09 17:14:46 +0100

    Jag upskattar inte play.py

--- ui_elements.py
+++ ui_elements.py
@@ -10,6 +10,48 @@ from emoji import emojize
 # text_rad3 = ""
 
 #--------------------------------------------------------------
+disclaimer = """
+Disclaimer for TLP
+If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at hbcownjfrbsktikvky@tmmwj.net.
+
+Disclaimers for Escape From Rimbo
+All the information on this website - escrimbo.com - is published in good faith and for general information purpose only. Escape From Rimbo does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (Escape From Rimbo), is strictly at your own risk. Escape From Rimbo will not be liable for any losses and/or damages in connection with the use of our website.
+
+From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.
+
+Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their "Terms of Service" before engaging in any business or uploading any information.
+
+Consent
+By using our website, you hereby consent to our disclaimer and agree to its terms.
+
+Update
+Should we update, amend or make any changes to this document, those changes will be prominently posted here."""
+
+thx_disclaimer = '''
+        ,----,                                                                                         
+      ,/   .`|                                                                 ,----..                 
+    ,`   .'  : ,---,                               ,-.                        /   /   \                
+  ;    ;     ,--.' |                           ,--/ /|                 ,---, /   .     :         ,--,  
+.'___,/    ,'|  |  :                    ,---,,--. :/ |                /_ ./|.   /   ;.  \      ,'_ /|  
+|    :     | :  :  :                ,-+-. /  :  : ' /           ,---, |  ' .   ;   /  ` ; .--. |  | :  
+;    |.';  ; :  |  |,--. ,--.--.   ,--.'|'   |  '  /           /___/ \.  : ;   |  ; \ ; ,'_ /| :  . |  
+`----'  |  | |  :  '   |/       \ |   |  ,"' '  |  :            .  \  \ ,' |   :  | ; | |  ' | |  . .  
+    '   :  ; |  |   /' .--.  .-. ||   | /  | |  |   \            \  ;  `  ,.   |  ' ' ' |  | ' |  | |  
+    |   |  ' '  :  | | |\__\/: . .|   | |  | '  : |. \            \  \    ''   ;  \; /  :  | | :  ' ;  
+    '   :  | |  |  ' | :," .--.; ||   | |  |/|  | ' \ \            '  \   | \   \  ',  /|  ; ' |  | '  
+    ;   |.'  |  :  :_:,/  /  ,.  ||   | |--' '  : |--'              \  ;  ;  ;   :    / :  | : ;  ; |  
+    '---'    |  | ,'  ;  :   .'   |   |/     ;  |,'                  :  \  \  \   \ .'  '  :  `--'   \ 
+             `--''    |  ,     .-.'---'      '--'                     \  ' ;   `---`    :  ,      .-./ 
+                       `--`---'                                        `--`              `--`----'     
+                                                                                                       
+'''
+
+#DrPepperStyle
+game_over=""" ___                     ___                
+/  _>  ___ ._ _ _  ___  | . | _ _  ___  _ _ 
+| <_/\<_> || ' ' |/ ._> | | || | |/ ._>| '_>
+`____/<___||_|_|_|\___. `___'|__/ \___.|_|  
+                                            """
 intro_name = '''
 ▄▄▄ ..▄▄ ·  ▄▄·  ▄▄▄·  ▄▄▄·▄▄▄ .  ·▄▄▄▄▄▄        • ▌ ▄ ·.   ▄▄▄  ▪  • ▌ ▄ ·. ▄▄▄▄·       
 ▀▄.▀·▐█ ▀. ▐█ ▌▪▐█ ▀█ ▐█ ▄█▀▄.▀·  ▐▄▄ ▀▄ █· ▄█▀▄ ·██ ▐███▪  ▀▄ █·██ ·██ ▐███▪▐█ ▀█▪ ▄█▀▄ 
